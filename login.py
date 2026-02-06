7e897926e67f5841f90841ada3a8ffe5158d13c0
Mr-Raza649 <mrraza649@gmail.com>
2026-02-06 21:49:51 +0500

    Code Updated

--- login.py
+++ login.py
@@ -8,3 +8,10 @@ def my_function(fname):
     print(fname +  " Login")
     
 my_function("Gmail")
+
+def exFun(fname, lname = "John"):
+    print(fname + " " + lname)
+    
+exFun("John", "Carter")
+exFun("Jasper")
+
