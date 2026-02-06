6907b023ebd9a238fab38b9d964f67ee8b90b4db
Mr-Raza649 <mrraza649@gmail.com>
2026-02-06 21:38:01 +0500

    Updated code

--- login.py
+++ login.py
@@ -1,4 +1,10 @@
+
 def func():
     print("This is login file code")
     
 func()
+
+def my_function(fname):
+    print(fname +  " Login")
+    
+my_function("Gmail")
--- testCode.py
+++ testCode.py
@@ -4,3 +4,13 @@ def func():
     print("This is a function in testCode.py")
 
 func()
+
+def myfunction():
+    pass
+
+
+def my_function(fname):
+    print(fname +  " Reference")
+    
+my_function("Email")
+my_function("Linus")
