e8d50597b8fa8265c90aebd3b7cc712c56955344
Mr-Raza649 <mrraza649@gmail.com>
2026-02-06 21:28:37 +0500

    login file ccreated and code updated

---
+++ login.py
@@ -0,0 +1,4 @@
+def func():
+    print("This is login file code")
+    
+func()
--- testCode.py
+++ testCode.py
@@ -1,1 +1,6 @@
 print("Hello test code from public-repo-0")
+
+def func():
+    print("This is a function in testCode.py")
+
+func()
