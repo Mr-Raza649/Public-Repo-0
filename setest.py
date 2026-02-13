56140cf72e094d9d183c3f6f704f1fc660555928
Mr-Raza649 <mrraza649@gmail.com>
2026-02-14 00:26:25 +0500

    Updated code

--- setest.py
+++ setest.py
@@ -1,8 +1,54 @@
-print("Main branch !")
+student_set = set()
+teacher_set = set()
 
-print("Hello I'm feature of feature 1 branch")
-print("I'm in the Feature one Branch !")
+def add_single_student():
+    try:
+        person_set = input("Which set to add student to? (student/teacher): ").lower()
+        name = input("Enter the name: ").strip()
+        if name:
+            if person_set == "student":
+                student_set.add(name)
+                print(f"\n{name} added to Student Set. \n")
+            elif person_set == "teacher":
+                teacher_set.add(name)
+                print(f"\n{name} added to teacher Set. \n")
+            else:
+                print("Invalid Set name: ")
+        else:
+            print("No name entered. ")
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+def view_all_students():
+    set_name = input("Enter set name to view all members (student/teacher): ")
+    try:
+        if set_name == "student":
+            for index, name in enumerate(student_set, 1):
+                print(f"{index}. {name}")
+        elif set_name == "teacher":
+            for index, name in enumerate(teacher_set, 1):
+                print(f"{index} {name}")
+        else:
+            print("Invalid set name")
+    except Exception as e:
+        print(f"An error occured: {e}")
 
-print("Feather one branch is updated !")
-
-print("Hello I'm feature of feature 1 branch")
+while True:
+    print("1. Add new students or teachers. ")
+    print("2. View all students or teachers. ")
+    print("3. Exit Program")
+    
+    choice = input("Enter number: ")
+    
+    if choice == "1":
+        add_single_student()
+    elif choice == "2":
+        view_all_students()
+    elif choice == "3":
+        print("Program Exit.... ")
+        break
+    else:
+        print("Invalid Number: ")
+        
+    
